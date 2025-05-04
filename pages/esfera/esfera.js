938ff2c6d3c8e326d2b91b13bb342df6cb918a99
guilhermewqz <gui7sampaio@gmail.com>
2025-05-04 14:56:16 -0700

    resolucao do exercicio esfera

---
+++ pages/esfera/esfera.js
@@ -0,0 +1,13 @@
+let pi = 3.14159;
+
+function volume(raio) {
+    return (4 / 3) * pi * (raio * raio * raio);
+}
+
+let volume1 = volume(3);
+let volume2 = volume(15);
+let volume3 = volume(1523);
+
+console.log(`VOLUME 1 = ${volume1.toFixed(3)}`);
+console.log(`VOLUME 2 = ${volume2.toFixed(3)}`);
+console.log(`VOLUME 3 = ${volume3.toFixed(3)}`);
